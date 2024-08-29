88bb5425b0e13c77b680fd01ba8344410655512a
Woo Hyuk Sung <tjddngur123@gmail.com>
2024-08-29 19:20:24 +0900

    퀵 정렬 추가

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -794,6 +794,43 @@ void MergeSort(vector<int>& v, int left, int right)
     MergeResult(v, left, mid, right);
 }
 #pragma endregion 
+
+#pragma region 퀵 정렬
+
+int Partition(vector<int>& v, int left, int right)
+{
+    int pivot = v[left];
+    int low = left + 1;
+    int high = right;
+
+    // O(N)
+    while (low <= high)
+    {
+        while (low <= right && pivot >= v[low])
+            low++;
+
+        while (high >= left && pivot <= v[high])
+            high--;
+
+        if (low < high)
+            swap(v[low], v[high]);
+    }
+
+    swap(v[left], v[high]);
+    return high;
+}
+
+void QuickSort(vector<int>& v, int left, int right)
+{
+    if (left > right)
+        return;
+
+    int pivot = Partition(v, left, right);
+    QuickSort(v, left, pivot - 1);
+    QuickSort(v, pivot + 1, right);
+}
+
+#pragma endregion
 int main()
 {
     vector<int> v;
