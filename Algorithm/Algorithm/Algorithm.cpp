42c994c536aa71b898b1f2512a25102e71c5830c
Woo Hyuk Sung <tjddngur123@gmail.com>
2024-08-29 21:20:59 +0900

    hash map

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -831,6 +831,115 @@ void QuickSort(vector<int>& v, int left, int right)
 }
 
 #pragma endregion
+
+#pragma region 해시 테이블
+
+// 해시 테이블
+
+// map vs hash_map(unordered_map)
+
+// map - Red-black Tree
+// 추가 탐색 삭제 - O(logN)
+
+// hash_map
+// 추가 탐색 삭제 - O(1)
+
+// 살은 내주고 뼈를 취한다. 메모리를 내주고 속도를 취한다.
+
+void TestTable()
+{
+    struct User
+    {
+        int userId = 0;
+        string username;
+    };
+
+    vector<User> users;
+    users.resize(1000);
+
+    users[777] = User{ 777, "Rookiss" };
+
+    string name = users[777].username;
+    cout << name << endl;
+
+    // 테이블
+    // 키를 알면 데이터를 단번에 찾을 수 있다.
+    // 만약 3억 이렇게 넘어가면 문제
+}
+
+// 보안
+// id : rr + pw : 1234
+// id : rr + pw : hash(1234) -> 12awerawero3j24
+
+// DB [rr][12awerawero3j24]
+
+void TestHash()
+{
+    struct User
+    {
+        int userId = 0;
+        string username;
+    };
+
+    vector<User> users;
+    users.resize(1000);
+
+    const int userId = 123456789;
+    int key = (userId % 1000); // hash <- 고유번호
+
+    // 123456789번 유저 정보 세팅
+    users[key] = User{ userId, "Rookiss" };
+
+    //123456789번 유저 이름
+    User& user = users[key];
+    if (user.userId == userId)
+    {
+        string name = user.username;
+        cout << name << endl;
+    }
+
+    // 충돌 문제
+    // 충돌이 발생한 자리를 대신해서 다른 빈자리를 찾아나선다
+    // 선형 조사법 (linear probing)
+    // hash(key)+1 -> hash(key)+2
+    // 이차 조사법 (quadratic probing)
+    // hash(key)+1^2 -> hash(key)+2^2
+    // -etc
+
+    // 체이닝
+}
+
+//O(1)
+void TestHashTableChaining()
+{
+    struct User
+    {
+        int userId = 0;
+        string username;
+    };
+
+    vector<vector<User>> users;
+    users.resize(1000);
+
+    const int userId = 123456789;
+    int key = (userId % 1000); // hash <- 고유번호
+
+    // 123456789번 유저 번호 세팅
+    users[key].push_back(User{ userId, "Rookiss" });
+    users[789].push_back(User{ 789, "Faker" });
+
+    // 123456789번 유저 이름은?
+    vector<User>& bucket = users[key];
+    for (User& user : bucket)
+    {
+        if (user.userId == userId)
+        {
+            string name = user.username;
+            cout << name << endl;
+        }
+    }
+}
+#pragma endregion
 int main()
 {
     vector<int> v;
