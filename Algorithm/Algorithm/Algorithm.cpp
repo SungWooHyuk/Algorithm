c3647fb8a5738813bc24a0e48e9ba79dc4aee7ad
Woo Hyuk Sung <tjddngur123@gmail.com>
2024-09-30 17:58:15 +0900

    DP Enchant

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -1166,103 +1166,131 @@ void TestHashTableChaining()
 
 // 0 ~ 3 ^ 9 = 19683
 
-int HashKey(const vector<vector<char>>& board)
+//int HashKey(const vector<vector<char>>& board)
+//{
+//	int ret = 0;
+//
+//	for (int y = 0; y < 3; ++y)
+//	{
+//		for (int x = 0; x < 3; ++x)
+//		{
+//			ret = ret * 3;
+//
+//			if (board[y][x] == 'o')
+//				ret += 1;
+//			else if (board[y][x] == 'x')
+//				ret += 2;
+//		}
+//	}
+//
+//	return ret;
+//}
+//
+//vector<vector<char>> board;
+//int cache[19683];
+//
+//bool IsFinished(const vector<vector<char>>& board, char turn)
+//{
+//	// 좌우
+//	for (int i = 0; i < 3; i++)
+//		if (board[i][0] == turn && board[i][1] == turn && board[i][2] == turn)
+//			return true;
+//
+//	// 상하
+//	for (int i = 0; i < 3; i++)
+//		if (board[0][i] == turn && board[1][i] == turn && board[2][i] == turn)
+//			return true;
+//
+//	// 대각선
+//	if (board[0][0] == turn && board[1][1] == turn && board[2][2] == turn)
+//		return true;
+//
+//	if (board[0][2] == turn && board[1][1] == turn && board[2][0] == turn)
+//		return true;
+//
+//	return false;
+//}
+//
+//enum
+//{
+//	DEFAULT = 2,
+//	WIN = 1,
+//	DRAW = 0,
+//	LOSE = -1
+//};
+//
+//int CanWin(vector<vector<char>>& board, char turn)
+//{
+//	// 기저사례
+//	if (IsFinished(board, 'o' + 'x' - turn))
+//		return LOSE;
+//
+//	// 캐시 확인
+//	int key = HashKey(board);
+//	int& ret = cache[key];
+//	if (ret != DEFAULT)
+//		return ret;
+//
+//	// 풀기
+//
+//	// [.][x][.]
+//	// [.][o][.]
+//	// [.][.][.]
+//	int minValue = DEFAULT;
+//
+//	for (int y = 0; y < 3; ++y)
+//	{
+//		for (int x = 0; x < 3; ++x)
+//		{
+//			if (board[y][x] != '.')
+//				continue;
+//
+//			// 착수
+//			board[y][x] = turn;
+//
+//			//확인
+//			minValue = min(minValue, CanWin(board, 'o' + 'x' - turn)); // 상대방이 패배했을때가 제일 좋은케이스
+//			
+//			//취소
+//			board[y][x] = '.';
+//		}
+//	}
+//
+//	if (minValue == DRAW || minValue == DEFAULT)
+//		return ret = DRAW;
+//
+//	return ret = -minValue; // 상대가 이기면 내가 지고 상대가 지면 내가 이김
+//}
+
+#pragma endregion
+
+#pragma region enchant DP
+
+// 집행검 +N강까지 도달하는경우의수
+// +1,+2,+3강이 가능하며 터지는건 경우의수에 없음
+
+int N;
+int cache[100];
+
+int Enchant(int num)
 {
-	int ret = 0;
 
-	for (int y = 0; y < 3; ++y)
-	{
-		for (int x = 0; x < 3; ++x)
-		{
-			ret = ret * 3;
+	//기저
+	if (num > N)
+		return 0; // 초과는 안셈
+	else if (num == N)
+		return 1; // 추가
 
-			if (board[y][x] == 'o')
-				ret += 1;
-			else if (board[y][x] == 'x')
-				ret += 2;
-		}
-	}
-
-	return ret;
-}
-
-vector<vector<char>> board;
-int cache[19683];
-
-bool IsFinished(const vector<vector<char>>& board, char turn)
-{
-	// 좌우
-	for (int i = 0; i < 3; i++)
-		if (board[i][0] == turn && board[i][1] == turn && board[i][2] == turn)
-			return true;
-
-	// 상하
-	for (int i = 0; i < 3; i++)
-		if (board[0][i] == turn && board[1][i] == turn && board[2][i] == turn)
-			return true;
-
-	// 대각선
-	if (board[0][0] == turn && board[1][1] == turn && board[2][2] == turn)
-		return true;
-
-	if (board[0][2] == turn && board[1][1] == turn && board[2][0] == turn)
-		return true;
-
-	return false;
-}
-
-enum
-{
-	DEFAULT = 2,
-	WIN = 1,
-	DRAW = 0,
-	LOSE = -1
-};
-
-int CanWin(vector<vector<char>>& board, char turn)
-{
-	// 기저사례
-	if (IsFinished(board, 'o' + 'x' - turn))
-		return LOSE;
-
-	// 캐시 확인
-	int key = HashKey(board);
-	int& ret = cache[key];
-	if (ret != DEFAULT)
+	//캐쉬
+	int& ret = cache[num];
+	if (ret != -1)
 		return ret;
 
-	// 풀기
-
-	// [.][x][.]
-	// [.][o][.]
-	// [.][.][.]
-	int minValue = DEFAULT;
-
-	for (int y = 0; y < 3; ++y)
-	{
-		for (int x = 0; x < 3; ++x)
-		{
-			if (board[y][x] != '.')
-				continue;
-
-			// 착수
-			board[y][x] = turn;
-
-			//확인
-			minValue = min(minValue, CanWin(board, 'o' + 'x' - turn)); // 상대방이 패배했을때가 제일 좋은케이스
-			
-			//취소
-			board[y][x] = '.';
-		}
-	}
-
-	if (minValue == DRAW || minValue == DEFAULT)
-		return ret = DRAW;
-
-	return ret = -minValue; // 상대가 이기면 내가 지고 상대가 지면 내가 이김
+	return ret = Enchant(num + 1) + Enchant(num + 2) + Enchant(num + 3);
 }
 
 #pragma endregion
+
 int main()
 {
 	{ // TRIANGLE_PATH
@@ -1292,29 +1320,8 @@ int main()
 			y++;
 		}*/
 	}
-
-	board = vector<vector<char>>
-	{
-		{'.', '.', '.'},
-		{'.', '.', '.'},
-		{'.', '.', '.'}
-	};
-
-	for (int i = 0; i < 19683; i++)
-		cache[i] = DEFAULT;
-
-	int win = CanWin(board, 'x');
-
-	switch (win)
-	{
-	case WIN:
-		cout << "Win" << endl;
-		break;
-	case DRAW:
-		cout << "Draw" << endl;
-		break;
-	case LOSE:
-		cout << "Lose" << endl;
-		break;
-	}
+	N = 9;
+	memset(cache, -1, sizeof(cache));
+	cout << Enchant(0) << endl;
+	return Enchant(0);
 }
